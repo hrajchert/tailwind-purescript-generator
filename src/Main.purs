3b867b1c351ff643849cc7d2931fd35dea972fc9
Hernan Rajchert <hrajchert@gmail.com>
2020-12-19 17:40:25 -0300

    Generate directional properties

--- src/Main.purs
+++ src/Main.purs
@@ -13,15 +13,16 @@ testModule :: Module
 testModule =
   { path: [ "Css", "Theme" ]
   , utilities:
-      [ generate "xs" (FontSize "0.75rem" "1rem")
-      , generate "sm" (FontSize "0.875rem" "1.25rem")
-      , generate "base" (FontSize "1rem" "1.5rem")
-      , generate "thin" (FontWeight "100")
-      , generate "extralight" (FontWeight "200")
-      , generate "light" (FontWeight "300")
-      , generate "0" (Padding "0px")
-      , generate "1" (Padding "0.25rem")
-      ]
+      join
+        [ generate "xs" (FontSize "0.75rem" "1rem")
+        , generate "sm" (FontSize "0.875rem" "1.25rem")
+        , generate "base" (FontSize "1rem" "1.5rem")
+        , generate "thin" (FontWeight "100")
+        , generate "extralight" (FontWeight "200")
+        , generate "light" (FontWeight "300")
+        , generate "0" (Padding "0px")
+        , generate "1" (Padding "0.25rem")
+        ]
   }
 
 outputDir :: FilePath
--- src/Tailwind.purs
+++ src/Tailwind.purs
@@ -6,6 +6,7 @@ import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..), split)
 import Generator (GeneratedUtility)
 import Utils.String (capitalize)
+import Data.Array (uncons)
 
 type PropertyName
   = String
@@ -21,57 +22,97 @@ type Size
 type Weight
   = String
 
+-- FIXME almost sure that propertyName (which may need a new name) should be
+-- between each Utility, as not all utilities have a """specification"""
 data Utility
   = FontSize Size Size
   | FontWeight Weight
   | Padding Size
 
-generateName :: UtilityName -> PropertyName -> { selector :: String, name :: String }
-generateName utilityName propertyName =
-  let
-    propertyParts = split (Pattern "-") propertyName
-  in
-    { selector: intercalate "-" $ [ utilityName ] <> propertyParts
-    , name: intercalate "" $ [ utilityName ] <> (capitalize <$> propertyParts)
-    }
+toParts :: String -> Array String
+toParts = split (Pattern "-")
 
-generate :: PropertyName -> Utility -> GeneratedUtility
+fromParts :: Array String -> String
+fromParts = intercalate "-"
+
+generateName :: Array String -> { selector :: String, name :: String }
+generateName names =
+  { selector: fromParts names
+  , name:
+      intercalate ""
+        $ case uncons names of
+            Nothing -> [] {- This should never happen, but maybe refactor to NonEmptyArray -}
+            Just { head, tail } -> [ head ] <> (capitalize <$> tail)
+  }
+
+generate :: PropertyName -> Utility -> Array GeneratedUtility
 generate propertyName (FontSize fontSize lineHeight) =
   let
-    { selector, name } = generateName "text" propertyName
+    { selector, name } = generateName $ [ "text" ] <> toParts propertyName
   in
-    { properties:
-        Just
-          [ { name: "font-size", value: fontSize }
-          , { name: "line-height", value: lineHeight }
-          ]
-    , selector
-    , name
-    }
+    [ { properties:
+          Just
+            [ { name: "font-size", value: fontSize }
+            , { name: "line-height", value: lineHeight }
+            ]
+      , selector
+      , name
+      }
+    ]
 
 generate propertyName (FontWeight weight) =
   let
-    { selector, name } = generateName "font" propertyName
+    { selector, name } = generateName $ [ "font" ] <> toParts propertyName
   in
-    { properties:
-        Just
-          [ { name: "font-weight", value: weight }
-          ]
-    , selector
-    , name
-    }
+    [ { properties:
+          Just
+            [ { name: "font-weight", value: weight }
+            ]
+      , selector
+      , name
+      }
+    ]
 
-generate propertyName (Padding size) =
-  let
-    { selector, name } = generateName "p" propertyName
-  in
-    { properties:
-        Just
-          [ { name: "padding", value: size }
-          ]
-    , selector
-    , name
-    }
+generate propertyName (Padding size) = generalProperty <> directionalProperties
+  where
+  generalProperty =
+    let
+      { selector, name } = generateName $ [ "p" ] <> toParts propertyName
+    in
+      [ { properties:
+            Just
+              [ { name: "padding", value: size }
+              ]
+        , selector
+        , name
+        }
+      ]
+
+  directionalProperties =
+    forAllDirections \directionModifier cssModifiers ->
+      let
+        { selector, name } = generateName $ [ "p", directionModifier ] <> toParts propertyName
+      in
+        { properties:
+            ( Just
+                ( cssModifiers
+                    <#> \cssModif ->
+                        { name: fromParts [ "padding", cssModif ], value: size }
+                )
+            )
+        , selector
+        , name
+        }
+
+forAllDirections :: (String -> Array String -> GeneratedUtility) -> Array GeneratedUtility
+forAllDirections generator =
+  [ generator "b" [ "bottom" ]
+  , generator "t" [ "top" ]
+  , generator "r" [ "right" ]
+  , generator "l" [ "left" ]
+  , generator "x" [ "left", "right" ]
+  , generator "y" [ "top", "bottom" ]
+  ]
 
 -- data Variant
 --   = Responsive
