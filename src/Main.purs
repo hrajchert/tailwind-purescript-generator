7901142897bdab226a57007a5b116ddb4ab7bafa
Hernan Rajchert <hrajchert@gmail.com>
2021-01-31 02:02:56 -0300

    refactor testModule into generateModule and testUtilities

--- src/Main.purs
+++ src/Main.purs
@@ -9,28 +9,30 @@ import Effect.Class (liftEffect)
 import Effect.Console as Console
 import Generator (GeneratedModule, writeModule)
 import Node.Path (FilePath)
-import Tailwind.Utility (Utility(..), generate)
 import Tailwind.Config (resolveConfig)
+import Tailwind.Utility (Utility(..), generate)
 
-testModule :: GeneratedModule
-testModule =
-  { path: [ "Css", "Theme" ]
-  , utilities:
-      join
-        [ generate (BackgroundColor [ "transparent" ] "transparent")
-        , generate (BackgroundColor [ "white" ] "#fff")
-        , generate (BackgroundColor [ "gray", "50" ] "#f9fafb")
-        , generate (BackgroundColor [ "gray", "100" ] "#f3f4f6")
-        , generate (FontSize [ "xs" ] "0.75rem" "1rem")
-        , generate (FontSize [ "sm" ] "0.875rem" "1.25rem")
-        , generate (FontSize [ "base" ] "1rem" "1.5rem")
-        , generate (FontWeight [ "thin" ] "100")
-        , generate (FontWeight [ "extralight" ] "200")
-        , generate (FontWeight [ "light" ] "300")
-        , generate (Padding [ "0" ] "0px")
-        , generate (Padding [ "1" ] "0.25rem")
-        , generate WordBreak
-        ]
+testUtilities :: Array Utility
+testUtilities =
+  [ (BackgroundColor [ "transparent" ] "transparent")
+  , (BackgroundColor [ "white" ] "#fff")
+  , (BackgroundColor [ "gray", "50" ] "#f9fafb")
+  , (BackgroundColor [ "gray", "100" ] "#f3f4f6")
+  , (FontSize [ "xs" ] "0.75rem" "1rem")
+  , (FontSize [ "sm" ] "0.875rem" "1.25rem")
+  , (FontSize [ "base" ] "1rem" "1.5rem")
+  , (FontWeight [ "thin" ] "100")
+  , (FontWeight [ "extralight" ] "200")
+  , (FontWeight [ "light" ] "300")
+  , (Padding [ "0" ] "0px")
+  , (Padding [ "1" ] "0.25rem")
+  , WordBreak
+  ]
+
+generateModule :: Array String -> Array Utility -> GeneratedModule
+generateModule path utilityList =
+  { path
+  , utilities: generate =<< utilityList
   }
 
 outputDir :: FilePath
@@ -43,7 +45,7 @@ main =
         void $ liftEffect
           $ spy "tailWind config"
           <$> resolveConfig "./tailwind-default-config.js"
-        writeModule outputDir testModule
+        writeModule outputDir $ generateModule [ "Css", "Theme" ] testUtilities
   where
   processResult = case _ of
     Left err -> Console.log $ "There was a problem: " <> message err
